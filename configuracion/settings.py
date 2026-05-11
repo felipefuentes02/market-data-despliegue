092a8141d96eb91b5c597d8b792d69de360139f6
felipfuentes02 <felipefuentes02@gmail.com>
2026-05-11 14:04:37 -0400

    cambio en stting, correccion url csrf

--- configuracion/settings.py
+++ configuracion/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = 'django-insecure-2t&l3app-8ofc5hc4s8_vw+#e3q4fg-17gqys!dolp!#-mp_g^
 DEBUG = False
 
 ALLOWED_HOSTS = ['market-data-servicio.onrender.com', 'localhost', '127.0.0.1']
-CSRF_TRUSTED_ORIGINS = ['https://market-data-ewy0.onrender.com']
+CSRF_TRUSTED_ORIGINS = ['https://market-data-servicio.onrender.com']
 
 
 # Application definition
